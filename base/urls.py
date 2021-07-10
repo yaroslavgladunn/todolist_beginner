0baed3e7597cf1b94090316ed5a5890a685e403d
yaroslavgladunn <debtyman@gmail.com>
2021-07-10 17:43:40 +0300

    created registration

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import Tasklist, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import Tasklist, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('login', CustomLoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(next_page='login'), name='logout'),
+    path('sign-up', RegisterPage.as_view(), name='sign-up'),
 
     path('', Tasklist.as_view(), name='tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task'),
